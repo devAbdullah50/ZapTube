8d8412b740d77d8611a95aa5099084f3b790218e
Abdullah <devabdullah50@gmail.com>
2025-06-17 15:33:15 +0000

    improve navbar

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,20 @@ export default function Home() {
   return (
     <>
       <section>
-        <div className="left flex">
-          <ul className="w-1/3 ">
+        <div>
+
+        <div className="left">
+          <ul className="w-1/3 bg-gray-800 text-white p-4">
               <li>Home</li>
               <li>Subscription</li>
               <li>ForYou</li>
               <li>Shorts</li>
           </ul>
         </div>
-        <div className="right"></div>
+        <div className="right w-2/3 bg-slate-800">
+         div.videos
+        </div>
+        </div>
       </section>
     </>
   );
