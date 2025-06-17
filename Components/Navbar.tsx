2ae4af8557022fd9d68445abce444fe71a12d8f1
Abdullah <devabdullah50@gmail.com>
2025-06-17 13:50:31 +0000

    Fix Navbar title and correct import path for Navbar component

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -3,7 +3,7 @@ export default function Navbar(){
         <nav className="bg-gray-800 p-4">
             <div className="container mx-auto flex justify-between items-center">
                 <div className="text-white text-lg font-bold">
-                    My Website
+                    ZapTube
                 </div>
                 <ul className="flex space-x-4">
                     <li><a href="#" className="text-gray-300 hover:text-white">Home</a></li>
