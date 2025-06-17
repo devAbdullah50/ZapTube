c63013a2213b7c2ee193209f6c2c1283d4c73636
Abdullah <devabdullah50@gmail.com>
2025-06-17 15:42:36 +0000

    make base of video section

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar(){
     return (
         <nav className="bg-gray-800 p-4">
-            <div className="container mx-auto flex justify-between items-center">
+            <div className="container mx-auto flex justify-between w-full items-center">
                 <div className="text-white text-lg font-bold">
                     ZapTube
                 </div>
